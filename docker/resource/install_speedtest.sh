f463299b486b498e451b0bc74593eceafe64bcf3
Developer <73163268+developer-ns@users.noreply.github.com>
2022-01-03 17:45:32 +0000

    Fixed install script and nordvpn-app bugs.

--- docker/resource/install_speedtest.sh
+++ docker/resource/install_speedtest.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 curl -s https://install.speedtest.net/app/cli/install.deb.sh | bash
+
+#Fix signed issue
+sed -i 's/\[.*\]/\[trusted=yes\]/g' /etc/apt/sources.list.d/ookla_speedtest-cli.list && apt-get update
+
 apt-get install -y speedtest
